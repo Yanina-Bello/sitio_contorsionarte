3f3f374613d27b83fd08fec53bafdd971d460914
Yanina Bello <yaninabello@live.com>
2023-10-02 20:19:29 -0300

    haciendo crud parte 2 tira error

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -21,4 +21,69 @@ router.get('/eliminar/:id', async (req, res, next) => {
 
 }); //cierra get eliminar
 
+//formulario alta novedad//
+
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  });
+});
+
+//fin form alta//
+
+//MODIFICAR NOVEDAD//
+router.get('/modificar/:id', async (req, res, next) => {
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  });
+});
+
+//modificar
+router.post('/modificar', async (req, res, next) => {
+  try {
+  let obj = {
+    titulo: req.body.titulo,
+    subtitulo: req.body.subtitulo,
+    cuerpo: req.body.cuerpo,
+  }
+
+  await novedadesModel.modificarNovedadById(obj, req.body.id);
+ res.redirect('/admin/novedades');
+  }
+  catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se modificó la novedad'
+    })
+  }
+})
+
+
+//capturar datos enviados por formulario//
+router.post('/agregar', async (req, res, next) => {
+  try {
+    if (req.body.titulo != "" && req.body.subtitulo != "" &&
+      req.body.cuerpo != "") {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('/admin/novedades')
+
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true, message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se cargó la novedad'
+    });
+  }
+});
+
 module.exports = router;

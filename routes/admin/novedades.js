6e1be9a7682b65a619e6b64635fab282e3102b08
Yanina Bello <yaninabello@live.com>
2023-10-02 17:55:27 -0300

    02-10conNovedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades()
   res.render('admin/novedades', {
     layout: 'admin/layout',
-    usuario: req.session.nombre
+    usuario: req.session.nombre,
+    novedades
   });
 });
+
+/*para eliminar una novedad*/
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
+
+}); //cierra get eliminar
 
 module.exports = router;

41bb7822ce90dd889782d9c6df10e8ddaf0adba7
Yanina Bello <yaninabello@live.com>
2023-10-09 20:49:34 -0300

    09-10 FUNCIONANDO CN IMG

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
-
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 router.get('/', async function (req, res, next) {
 
-  var novedades = await novedadesModel.getNovedades()
+  var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        width: 50,
+        height: 50,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: ''
+      }
+    }
+  });
+
+
   res.render('admin/novedades', {
     layout: 'admin/layout',
     usuario: req.session.nombre,
@@ -15,6 +39,11 @@ router.get('/', async function (req, res, next) {
 /*para eliminar una novedad*/
 router.get('/eliminar/:id', async (req, res, next) => {
   var id = req.params.id;
+
+  let novedad = await novedadesModel.getNovedadById(id);
+  if (novedad.img_id) {
+    await (destroy(novedad.img_id));
+  }
 
   await novedadesModel.deleteNovedadesById(id);
   res.redirect('/admin/novedades')
@@ -44,10 +73,27 @@ router.get('/modificar/:id', async (req, res, next) => {
 //modificar
 router.post('/modificar', async (req, res, next) => {
   try {
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+
+    if (req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await (destroy(req.body.img_original));
+    }
     let obj = {
       titulo: req.body.titulo,
       subtitulo: req.body.subtitulo,
-      cuerpo: req.body.cuerpo
+      cuerpo: req.body.cuerpo,
+      img_id
     }
 
     await novedadesModel.modificarNovedadById(obj, req.body.id);
@@ -66,12 +112,21 @@ router.post('/modificar', async (req, res, next) => {
 //capturar datos enviados por formulario//
 router.post('/agregar', async (req, res, next) => {
   try {
+    var img_id = '';
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
     if (req.body.titulo != "" && req.body.subtitulo != "" &&
       req.body.cuerpo != "") {
-      await novedadesModel.insertNovedad(req.body);
+      await novedadesModel.insertNovedad({
+        ...req.body,
+        img_id
+      });
       res.redirect('/admin/novedades')
+    }
 
-    } else {
+    else {
       res.render('admin/agregar', {
         layout: 'admin/layout',
         error: true, message: 'Todos los campos son requeridos'
